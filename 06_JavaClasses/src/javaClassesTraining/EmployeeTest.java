825a87999a58a40fd59ffde39c9776cc45fde536
James <james.proctor@informed.com>
2019-01-09 15:17:35 +0000

    Created jUnit5 test case through UI that tests 10 methods - not yet implimented

---
+++ 06_JavaClasses/src/javaClassesTraining/EmployeeTest.java
@@ -0,0 +1,59 @@
+package javaClassesTraining;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	@Test
+	void testPayRise() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSalary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSalary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetJoined() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetJoined() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetID() {
+		fail("Not yet implemented");
+	}
+
+}
