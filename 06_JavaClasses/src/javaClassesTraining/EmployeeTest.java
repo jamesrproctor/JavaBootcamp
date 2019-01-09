0e703590c0541fc7be975e936d632d5cf0a3c646
James <james.proctor@informed.com>
2019-01-09 16:23:11 +0000

    Added working jUnit test cases for Employee method

--- 06_JavaClasses/src/javaClassesTraining/EmployeeTest.java
+++ 06_JavaClasses/src/javaClassesTraining/EmployeeTest.java
@@ -1,10 +1,19 @@
 package javaClassesTraining;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class EmployeeTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
 
 	@Test
 	void testPayRise() {
--- 06_JavaClasses/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author training
- *
- */
-module javaClassesTraining {
-}
