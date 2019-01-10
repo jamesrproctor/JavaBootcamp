19e09ccf14fb165ad0ba932266ce827b326adb2c
James <james.proctor@informed.com>
2019-01-10 09:47:44 +0000

    iniital commit for inheritance work

---
+++ 07_Inheritance/src/MemberTest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+	
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAge() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testSetAge() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetBorrowed() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testSetBorrowed() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testAddBorrowedItems() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testReturnBorrowedItems() {
+		fail("Not yet implemented");
+	}
+
+}
