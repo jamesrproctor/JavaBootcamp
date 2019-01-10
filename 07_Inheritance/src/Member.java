06e4c3804cf8deafe5830386139d974759dea1b9
James <james.proctor@informed.com>
2019-01-10 10:00:30 +0000

    Added init method for member class, added basic get methods

--- 07_Inheritance/src/Member.java
+++ 07_Inheritance/src/Member.java
@@ -1,4 +1,42 @@
 
 public class Member {
+	
+	// Class vars
+	private String name;
+	private int age;
+	private int borrowed;
+	
+	private int maxBorrowed = 12;
+	private int minAge = 11;
+	
+	// Constructor / init method
+	public Member(String name, int age) {
+		this.name = name;
+		if (age < minAge) {
+			this.age = minAge;
+		} else {
+			this.age = age;
+		}
+	}
+	
+	// Get methods
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public int getBorrowed() {
+		return borrowed;
+	}
+	
+	
+	// Set methods
+	
+		
+	
 
 }
