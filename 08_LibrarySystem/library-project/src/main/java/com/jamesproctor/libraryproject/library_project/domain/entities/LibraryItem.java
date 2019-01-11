bcfe4c2284b3dff52a5960e6fb3fe5ea5f5c5d9a
James <james.proctor@informed.com>
2019-01-11 09:15:22 +0000

    Added get() methods to libraryitem class

--- 08_LibrarySystem/library-project/src/main/java/com/jamesproctor/libraryproject/library_project/domain/entities/LibraryItem.java
+++ 08_LibrarySystem/library-project/src/main/java/com/jamesproctor/libraryproject/library_project/domain/entities/LibraryItem.java
@@ -1,5 +1,43 @@
 package com.jamesproctor.libraryproject.library_project.domain.entities;
 
-public class LibraryItem {
+import java.util.*;
+
+public abstract class LibraryItem {
+	
+	final String uuid = UUID.randomUUID().toString().replace("-", "");
+	
+	private String title;
+	
+	private Date dateAdded;
+	private Date dateBorrowed;
+	
+	private int totalCopies;
+	private int availableCopies;
+	private int borrowedCopies;
+	
+	// Class methods - can only view data but not change it!
+	public String getTitle() {
+		return title;
+	}
+
+	public Date getDateAdded() {
+		return dateAdded;
+	}
+
+	public Date getDateBorrowed() {
+		return dateBorrowed;
+	}
+
+	public int getTotalCopies() {
+		return totalCopies;
+	}
+
+	public int getAvailableCopies() {
+		return availableCopies;
+	}
+
+	public int getBorrowedCopies() {
+		return borrowedCopies;
+	}
 
 }
